582dcfed4c3f6ac79ae54cd8b97cd7d5f37b1f70
tvanzelderen <103991671+TvanZelderen@users.noreply.github.com>
2024-03-28 09:44:55 +0100

    Creation of dev, initial data of aircraft added.

---
+++ fokker_100.py
@@ -0,0 +1,16 @@
+TOTAL_LENGTH = 35.538 # m - janes
+TOTAL_HEIGHT = 8.419 # m - janes
+B = 28.076 # m - janes
+S = 93.5 # m^2 - wikipedia
+FUSELAGE_LENGTH = 23.5 # m -janes
+FUSELAGE_WIDTH = 3.3 # m - janes
+WHEELBASE = 14.006 # m - janes
+WHEEL_TRACK = 5.040 # m - janes
+HORIZONTAL_STAB_B = 10.040 # m - janes
+
+MTOW = 45810 # kg - janes
+MLW = 39915 # kg - janes
+MZFW = 36740 # kg - janes
+FUEL_CAPACITY = 10731 # kg - janes
+MAX_PAYLOAD = 11993 # kg - wikipedia
+EMPTY_WEIGHT = 24541 # kg - wikipedia
