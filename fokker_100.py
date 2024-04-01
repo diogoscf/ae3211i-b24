ba2b78e27590af479c409864e2a284bc79f99c1f
Diogo S.C. Fernandes <diogo.scf2003@gmail.com>
2024-04-01 09:54:24 +0200

    Move stuff to master (#2)
    * Creation of dev, initial data of aircraft added.
    
    * fixes fokker_100.py
    
    ---------
    
    Co-authored-by: tvanzelderen <103991671+TvanZelderen@users.noreply.github.com>
    Co-authored-by: Alexis van den Heede <105492216+AlexisVandenHeede@users.noreply.github.com>

---
+++ fokker_100.py
@@ -0,0 +1,16 @@
+TOTAL_LENGTH = 35.528 # m - janes
+TOTAL_HEIGHT = 8.419 # m - janes
+B = 28.076 # m - janes
+S = 93.5 # m^2 - wikipedia
+FUSELAGE_LENGTH = 32.501 # m -janes & Fokker
+FUSELAGE_WIDTH = 3.300 # m - janes & Fokker
+WHEELBASE = 14.006 # m - janes & Fokker
+WHEEL_TRACK = 5.040 # m - janes & Fokker
+HORIZONTAL_STAB_B = 10.040 # m - janes & Fokker
+
+MTOW = 45810 # kg - janes
+MLW = 39915 # kg - janes
+MZFW = 36740 # kg - janes
+FUEL_CAPACITY = 10731 # kg - janes
+MAX_PAYLOAD = 11993 # kg - wikipedia
+EMPTY_WEIGHT = 24541 # kg - wikipedia
