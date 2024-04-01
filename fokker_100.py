41914b5e9895e610e18949e7620bf0d0d003362e
Alexis van den Heede <105492216+AlexisVandenHeede@users.noreply.github.com>
2024-04-01 09:25:07 +0200

    fixes fokker_100.py

--- fokker_100.py
+++ fokker_100.py
@@ -1,12 +1,12 @@
-TOTAL_LENGTH = 35.538 # m - janes
+TOTAL_LENGTH = 35.528 # m - janes
 TOTAL_HEIGHT = 8.419 # m - janes
 B = 28.076 # m - janes
 S = 93.5 # m^2 - wikipedia
-FUSELAGE_LENGTH = 23.5 # m -janes
-FUSELAGE_WIDTH = 3.3 # m - janes
-WHEELBASE = 14.006 # m - janes
-WHEEL_TRACK = 5.040 # m - janes
-HORIZONTAL_STAB_B = 10.040 # m - janes
+FUSELAGE_LENGTH = 32.501 # m -janes & Fokker
+FUSELAGE_WIDTH = 3.300 # m - janes & Fokker
+WHEELBASE = 14.006 # m - janes & Fokker
+WHEEL_TRACK = 5.040 # m - janes & Fokker
+HORIZONTAL_STAB_B = 10.040 # m - janes & Fokker
 
 MTOW = 45810 # kg - janes
 MLW = 39915 # kg - janes
