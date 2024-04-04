9de2eecfb2df842c8aeb62703e71b2fa9edcc70c
Jeremy Smith <64465231+pt1243@users.noreply.github.com>
2024-04-04 22:04:04 +0200

    scissor plot code

--- scissor_plot.py
+++ scissor_plot.py
@@ -1,5 +1,6 @@
 from math import tan, radians, sqrt, pi, atan2, sin, cos
 import matplotlib.pyplot as plt
+from constants import G
 
 
 def datcom(A, M, eta, half_chord_sweep):
@@ -9,7 +10,7 @@ def datcom(A, M, eta, half_chord_sweep):
 
 
 def stability_line(
-    ax, max_Sh_S, M, S, S_net, b, mac, quarter_chord_sweep, half_chord_sweep, taper_ratio, A_h,
+    ax, max_Sh_S, M, S, S_net, b, c, quarter_chord_sweep, half_chord_sweep, taper_ratio, A_h,
     horizontal_stab_half_chord_sweep, use_torenbeek_method, eta, l_h, delta_z_wing_tail_acs, b_f, h_f, l_fn, b_n, l_n, 
     c_l_des, x_ac_w, k_n, Vh_V, stability_margin,
 ):
@@ -47,25 +48,70 @@ def stability_line(
         d_eps_d_alpha = 1.75 * C_L_alpha_w / (pi * A * (taper_ratio * r)**0.25 * (1 + abs(m)))
         one_minus_d_eps_d_alpha = (1 - d_eps_d_alpha) * 0.9  # correct for fuselage engines
     
+    one_minus_d_eps_d_alpha = 1 - 0.10
 
-    x_ac_f1 = (-1.8 / C_L_alpha_A_H) * (b_f * h_f * l_fn) / (S * mac)
+    x_ac_f1 = (-1.8 / C_L_alpha_A_H) * (b_f * h_f * l_fn) / (S * c)
     c_g = S / b
-    x_ac_f2 = (0.273 / (1 + taper_ratio)) * (b_f * c_g * (b - b_f)) / (mac ** 2 * (b + 2.15 * b_f)) * tan(quarter_chord_sweep)
-    x_ac_n = 2 * k_n * (b_n ** 2 * l_n) / (S * mac * C_L_alpha_A_H)
+    x_ac_f2 = (0.273 / (1 + taper_ratio)) * (b_f * c_g * (b - b_f)) / (c ** 2 * (b + 2.15 * b_f)) * tan(quarter_chord_sweep)
+    x_ac_n = 2 * k_n * (b_n ** 2 * l_n) / (S * c * C_L_alpha_A_H)
     x_ac = x_ac_w + x_ac_f1 + x_ac_f2 + x_ac_n
 
-    denom = (C_L_alpha_H / C_L_alpha_A_H * one_minus_d_eps_d_alpha * l_h / mac * Vh_V ** 2)
+    denom = (C_L_alpha_H / C_L_alpha_A_H * one_minus_d_eps_d_alpha * l_h / c * Vh_V ** 2)
     intercept = (x_ac - stability_margin) / denom
-    x_points = [x_ac - stability_margin, (max_Sh_S - intercept) * denom]
+    slope = 1 / denom
+    x_points = [x_ac - stability_margin, (max_Sh_S - intercept) / slope]
     y_points = [0, max_Sh_S]
     # breakpoint()
     ax.plot(x_points, y_points, label="Stability curve")
 
 
+def controllability_line(
+    ax, max_Sh_S, M, alpha_0_l, delta_alpha_0_l, C_m_0_airfoil, C_l_max, eta, S, S_prime, S_wf, c, b, b_f, h_f, l_f,
+    l_h, x_ac, quarter_chord_sweep, half_chord_sweep, flap_hinge_sweep_angle, V_app, rho, W, Vh_V, c_prime, C_L_H, mu_1,
+    mu_2, mu_3,
+):
+    A = b**2 / S
+    C_L_alpha_A_H = datcom(A, M, eta, half_chord_sweep)
+    alpha_0_L = alpha_0_l
+    delta_alpha_0_L = delta_alpha_0_l * (S_wf / S) * cos(flap_hinge_sweep_angle)
+    C_L_alpha_clean = C_L_alpha_A_H
+    C_L_alpha_flapped = S_prime / S * C_L_alpha_clean
+    C_L_alpha_equals_0 = radians(abs(alpha_0_L - delta_alpha_0_L)) * C_L_alpha_flapped
+    
+    C_m_ac_w = C_m_0_airfoil * (A * cos(quarter_chord_sweep)**2) / (A + 2*cos(quarter_chord_sweep))
+    delta_fus_C_m_ac = -1.8 * (1 - 2.5*b_f/l_f) * (pi * b_f * h_f * l_f) / (4 * S * c) * (C_L_alpha_equals_0 / C_L_alpha_A_H)
+    delta_nac_C_m_ac = 0
+
+    delta_f_c_l = 1.6 * c_prime / c
+    C_L = sqrt(2/rho * W/S * 1 / V_app**2)
+    c_l = C_L - delta_f_c_l * (1 - S_wf / S)
+    delta_f_c_m_1_4 = -mu_1 * delta_f_c_l * (c_prime / c) - c_l * (1/8) * (c_prime / c) * (c_prime / c - 1)
+    delta_f_C_m_ac = mu_2 * delta_f_c_m_1_4 + 0.7 * A / (1 + 2/A) * mu_3 * delta_f_c_l * tan(quarter_chord_sweep)
+    C_m_ac = C_m_ac_w + delta_f_C_m_ac + delta_fus_C_m_ac + delta_nac_C_m_ac
+
+    delta_C_l_max = delta_f_c_l
+    delta_C_L_max = 0.9 * delta_C_l_max * (S_wf / S) * cos(flap_hinge_sweep_angle)
+    C_L_max_clean = 0.9 * C_l_max * cos(quarter_chord_sweep)
+    C_L_max = C_L_max_clean + delta_C_L_max
+    V_s = V_app / 1.3
+    C_L_landing = (V_s / V_app)**2 * C_L_max
+    C_L_w = C_L_landing
+    C_L_A_H = C_L_w
+
+    denom = (C_L_H / C_L_A_H) * (l_h / c) * Vh_V ** 2
+    intercept = (C_m_ac / C_L_A_H - x_ac) / denom
+    slope = 1 / denom
+    x_points = [(max_Sh_S - intercept) / slope, -intercept / slope]
+    y_points = [intercept + max_Sh_S, 0]
+    ax.plot(x_points, y_points, label="Controllability curve")
+
+
 def scissor_plot(
     filename, show_legend, max_Sh_S, M_cruise, S, S_net, b, mac, quarter_chord_sweep_deg, half_chord_sweep_deg,
     taper_ratio, A_h, horizontal_stab_half_chord_sweep_deg, use_torenbeek_method, eta, l_h, delta_z_wing_tail_acs, b_f,
-    h_f, l_fn, b_n, l_n, c_l_des, x_ac_w, k_n, Vh_V, stability_margin,
+    h_f, l_fn, b_n, l_n, c_l_des, x_ac_w_cruise, k_n, Vh_V, stability_margin, V_app_kts, alpha_0_l, delta_alpha_0_l,
+    C_m_0_airfoil, C_l_max, S_prime, S_wf, l_f, x_ac_w_landing, flap_hinge_sweep_deg, rho_landing, m_landing_kg,
+    delta_c_cf, cf, C_L_H, mu_1, mu_2, mu_3,
 ):
     fig, ax = plt.subplots(figsize=(12, 8))
 
@@ -92,10 +138,43 @@ def scissor_plot(
         b_n,
         -l_n,
         c_l_des,
-        x_ac_w,
+        x_ac_w_cruise,
         k_n,
         Vh_V,
         stability_margin,
+    )
+
+    controllability_line(
+        ax,
+        max_Sh_S,
+        V_app_kts * 1852/3600 / sqrt(1.4 * 287 * 288.15),
+        alpha_0_l,
+        delta_alpha_0_l,
+        C_m_0_airfoil,
+        C_l_max,
+        eta,
+        S,
+        S_prime,
+        S_wf,
+        mac,
+        b,
+        b_f,
+        h_f,
+        l_f,
+        l_h,
+        x_ac_w_landing,
+        radians(quarter_chord_sweep_deg),
+        radians(half_chord_sweep_deg),
+        radians(flap_hinge_sweep_deg),
+        V_app_kts * 1852/3600,
+        rho_landing,
+        m_landing_kg * G,
+        Vh_V,
+        mac + delta_c_cf*cf,
+        C_L_H,
+        mu_1,
+        mu_2,
+        mu_3,
     )
 
     if show_legend:
@@ -133,9 +212,27 @@ if __name__ == "__main__":
         "b_n": 1.52530611,                                # max nacelle diameter [m]
         "l_n": 10.32898502,                               # distance from wing a.c. to rear of nacelle [m]; should be positive
         "c_l_des": 3 * 0.15,                              # airfoil design lift coefficient [-]
-        "x_ac_w": 0.445,                                  # from Torenbeek figure E-10 [-]
+        "x_ac_w_cruise": 0.445,                           # from Torenbeek figure E-10 [-]
         "k_n": -2.5,                                      # nacelle a.c. shift coefficient [-]; -2.5 for rear fuselage podded engines
         "Vh_V": 1,                                        # tail speed ratio [-]; 1 for T-tail
         "stability_margin": 0.05,                         # stability margin, in fraction m.a.c. [-]
+        "V_app_kts": 130,                                 # approach speed in [kts]
+        "alpha_0_l": -0.78,                               # airfoil angle of attack at which C_l = 0, [deg]
+        "delta_alpha_0_l": -15,                           # change in airfoil C_l = 0 in [deg], approx. -15 deg from ADSEE II
+        "C_m_0_airfoil": -0.013,                          # airfoil C_m when C_l = 0, [-]
+        "C_l_max": 1.618,                                 # airfoil max C_l, [-]
+        "S_prime": 98.2905,                               # entire surface of new wing when flaps are extended, should be >S, [m]
+        "S_wf": 26.8456 * 2,                              # portion of unflapped wing in front of flaps, should be <S, [m]
+        "l_f": 32.501,                                    # fuselage length [m]
+        "x_ac_w_landing": 0.355,                          # from Torenbeek figure E-10 [-]
+        "flap_hinge_sweep_deg": 6.59,                     # sweep of flap hinge line, [deg]
+        "rho_landing": 1.225,                             # air density at landing, [kg/m^3]
+        "m_landing_kg": 39915,                            # landing mass in [kg]
+        "delta_c_cf": 0.79,                               # read from Torenbeek fig. G-7 [-]
+        "cf": 1.250,                                      # flap chord as shown in Torenbeek fig. G-7, [m]
+        "C_L_H": -0.85,                                   # C_L of the tail at landing, -0.85 for adjustable tails [-]
+        "mu_1": 0.12,                                     # from Torenbeek fig. G-15
+        "mu_2": 0.78,                                     # from Torenbeek fig. G-16
+        "mu_3": 0.0575,                                   # from Torenbeek fig. G-17
     }
     scissor_plot(**reference_aircraft_config)
